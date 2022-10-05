a456f2f0fed14e2eab37dda2e05f15a227a58af6
dallier <teo.dallier@ecole.ensicaen.fr>
2022-10-05 17:03:25 +0200

    Added Shoreline to the IMapElement interface

--- src/main/java/fr/ensicaen/Elgama/model/IMapElement.java
+++ src/main/java/fr/ensicaen/Elgama/model/IMapElement.java
@@ -2,6 +2,6 @@ package fr.ensicaen.Elgama.model;
 
 import java.awt.geom.Point2D;
 
-public interface MapElement {
+public interface IMapElement {
     boolean isColliding(Point2D from, Point2D to);
 }
