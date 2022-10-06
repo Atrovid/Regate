50f55d68ec658e604cbdac12b6bc5a9310335b45
KylianPi <kylian.picque@ecole.ensicaen.fr>
2022-10-07 00:47:09 +0200

    function to cacul angle between wind and boat / test

--- src/test/java/fr/ensicaen/Elgama/model/BoatModelTest.java
+++ src/test/java/fr/ensicaen/Elgama/model/BoatModelTest.java
@@ -11,27 +11,53 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public final class BoatModelTest {
 
     private static Point2D boat_point ;
+    private static Point2D secondBoat_point;
     private static BoatModel boat ;
+    private static BoatModel secondBoat;
+    private static  BoatModel thirdBoat;
 
     @BeforeAll
     public static void oneTimeSetUp(){
         boat_point = new Point2D.Float(200,190);
-        boat = new BoatModel();
+        secondBoat_point = new Point2D.Float(0,-1);
 
+        boat = new BoatModel();
+        secondBoat = new BoatModel();
+        thirdBoat = new  BoatModel();
 
     }
 
     @Test
     public void testScalar(){
-        assertEquals(0,boat.scalar(boat_point));
+        assertEquals(0,boat.scalarProduct(boat_point));
         boat.rotate(90);
         boat.move();
-        assertEquals(200, boat.scalar(boat_point));
+
+        assertEquals(200, boat.scalarProduct(boat_point));
 
         boat.rotate(90);
         boat.move();
-        assertEquals(190, boat.scalar(boat_point),0.0001);
+        assertEquals(190,Math.round(boat.scalarProduct(boat_point)));
+    }
+
+    @Test
+    public void testVectorialBetweenVectors(){
+        assertEquals(0.0, secondBoat.productBetweenNorm(boat_point));
+
+        secondBoat.rotate(56);
+        secondBoat.move();
+
+        assertEquals(275.86,secondBoat.productBetweenNorm(boat_point),0.1);
+
+
+        //assertEquals(0.0,secondBoat.vectorialBetweenVectors(secondBoat_point));
     }
 
 
+    @Test
+    public void testAngleBetweenWindAndBoat()
+    {
+        thirdBoat.move();
+        assertEquals( 0.0, thirdBoat.angleBetweenWindAndBoat( secondBoat_point ) );
+    }
 }
