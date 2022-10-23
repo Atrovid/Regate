d8366ac19fe22a4f9d6ad0d5e5a0a71f97e14bb4
Atrovid <sguo.david@gmail.com>
2022-10-23 15:18:00 +0200

    Changed IMapElement class name into IBoardElement class name

---
+++ src/main/java/fr/ensicaen/Elgama/model/IBoardElement.java
@@ -0,0 +1,11 @@
+package fr.ensicaen.Elgama.model;
+
+import fr.ensicaen.Elgama.model.game_board.IBoardElementVisitor;
+
+import java.awt.geom.Point2D;
+
+public interface IBoardElement {
+    boolean isColliding(Point2D from, Point2D to);
+    Object accept(IBoardElementVisitor visitor, Object o);
+
+}
