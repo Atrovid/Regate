5571904f2424a69a90cc28d221f90ed9d76494a6
KylianPi <kylian.picque@ecole.ensicaen.fr>
2022-10-26 19:31:09 +0200

    #64 looks to be good, but not included yet

--- src/main/java/fr/ensicaen/elgama/model/sailboat/Sailboat.java
+++ src/main/java/fr/ensicaen/elgama/model/sailboat/Sailboat.java
@@ -4,19 +4,28 @@ import fr.ensicaen.elgama.model.game_board.Board;
 import javafx.geometry.Point2D;
 
 public class Sailboat  {
-    private Board _board;
-    private SailboatPolar _sailboatPolar;
-    private SailboatNavigation _sailboatNavigation;
+    private final SailboatNavigation _sailboatNavigation;
 
     public Sailboat( Board board, PolarReader.PolarType polarType ) {
-        _board = board;
-        _sailboatPolar = new SailboatPolar( polarType );
+//        _board = board;
+//        _sailboatPolar = new SailboatPolar( polarType );
         _sailboatNavigation = new SailboatNavigation( board, polarType );
     }
 
-//    public void setDirection(Point2D direction ) {
-//        _sailboatNavigation.setDirection( direction );
-//    }
+    public void move() {
+        _sailboatNavigation.moveForward();
+    }
+
+    public Point2D getPosition() {
+        return _sailboatNavigation.getPosition();
+    }
+
+    public Point2D getDirection() {
+        return _sailboatNavigation.getDirection();
+    }
+    public void setDirection( double angle ) {
+        _sailboatNavigation.setDirection( angle );
+    }
 
 
 }
--- src/main/java/fr/ensicaen/elgama/model/sailboat/SailboatNavigation.java
+++ src/main/java/fr/ensicaen/elgama/model/sailboat/SailboatNavigation.java
@@ -7,25 +7,33 @@ import javax.swing.*;
 
 public class SailboatNavigation {
     private Point2D _position = new Point2D( 580, 480);
-    private Point2D _direction = new Point2D( 0, 0);
-    private Board _board;
-    private PolarReader.PolarType _polarType;
-    private SailboatPolar _sailboatPolar;
+    private Point2D _direction;
+    private final Board _board;
+
+    private final SailboatPolar _sailboatPolar;
 
     public SailboatNavigation( Board board, PolarReader.PolarType polarType ) {
         _board = board;
-        _polarType = polarType;
+//        _polarType = polarType;
         _sailboatPolar = new SailboatPolar( polarType );
-    }
-
-    public void setDirection( double angle ) {
-        _direction = new Point2D(Math.sin(angle * Math.PI / 180), -Math.cos(angle * Math.PI / 180));
     }
 
     public Point2D getPosition() {return _position;}
 
     public Point2D getDirection() {return _direction;}
 
+    public void setDirection( double angle ) {
+        _direction = new Point2D(Math.sin(angle * Math.PI / 180), -Math.cos(angle * Math.PI / 180));
+        _direction = _direction.multiply(
+                                        _sailboatPolar.getMaxSpeed(
+                                                        angle,
+                                                        _board.getWindStrength()
+                )
+        );
+    }
 
+    public void moveForward() {
 
+        _position = _position.add( _direction );
+    }
 }
