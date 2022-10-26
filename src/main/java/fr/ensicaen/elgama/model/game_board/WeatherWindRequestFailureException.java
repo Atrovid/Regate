15ccea05d9d2757fedd0c6a809f7773c3fe690ad
Gaudron-parry Elliot <elliot.gaudron-parry@ecole.ensicaen.fr>
2022-10-26 15:18:07 +0200

    WheaterWind class terminated and tested

---
+++ src/main/java/fr/ensicaen/elgama/model/game_board/WeatherWindRequestFailureException.java
@@ -0,0 +1,7 @@
+package fr.ensicaen.elgama.model.game_board;
+
+public class WeatherWindRequestFailureException extends Exception {
+    public WeatherWindRequestFailureException(String message) {
+        super(message);
+    }
+}
